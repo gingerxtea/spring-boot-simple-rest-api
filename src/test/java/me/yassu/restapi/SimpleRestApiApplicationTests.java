beda13e4baca29a30b0494713bbc08d3cb94f9e4
gingerxtea <yassu7221@gmail.com>
2026-09-16 01:31:30 +0530

    comment out spring boot context load tests

--- src/test/java/me/yassu/restapi/SimpleRestApiApplicationTests.java
+++ src/test/java/me/yassu/restapi/SimpleRestApiApplicationTests.java
@@ -3,11 +3,7 @@ package me.yassu.restapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SimpleRestApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
